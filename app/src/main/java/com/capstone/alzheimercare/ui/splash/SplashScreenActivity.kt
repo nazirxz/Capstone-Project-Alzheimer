234694e417005a7434a7c449d196b6bb89bab744
Nazirman <nzryanzr@gmail.com>
2022-06-06 16:12:25 +0700

    fixing splash screen login according to role

--- app/src/main/java/com/capstone/alzheimercare/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/capstone/alzheimercare/ui/splash/SplashScreenActivity.kt
@@ -3,18 +3,43 @@ package com.capstone.alzheimercare.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.alzheimercare.R
+import com.capstone.alzheimercare.ui.CaretakerActivity
+import com.capstone.alzheimercare.ui.MyPreference
+import com.capstone.alzheimercare.ui.PatientActivity
 import com.capstone.alzheimercare.ui.onboarding.OnboardingActivity
 
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var preference: MyPreference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        preference = MyPreference(this)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, OnboardingActivity::class.java))
+        window.setFlags(
+            FULL_SCREEN_FLAG,
+            FULL_SCREEN_FLAG
+        )
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (preference.getId() == "") {
+                startActivity(Intent(this, OnboardingActivity::class.java))
+            } else {
+                when (preference.getRole()) {
+                    "Patient" -> startActivity(Intent(this, PatientActivity::class.java))
+                    "Caretaker" -> startActivity(Intent(this, CaretakerActivity::class.java))
+                }
+            }
             finish()
-        }, 3000)
+        }, TIME)
+    }
+
+    companion object {
+        private const val TIME = 3000L
+        private const val FULL_SCREEN_FLAG = 1024
     }
 }
